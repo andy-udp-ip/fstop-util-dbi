ca4dc8d4999970d675b6308f670aebc89f7637c0
andy <andy.udp.ip@gmail.com>
2017-08-18 12:52:51 +0800

    update pom.xml

--- src/main/java/tw/com/fstop/App.java
+++ src/main/java/tw/com/fstop/App.java
@@ -1,5 +1,9 @@
 package tw.com.fstop;
 
+import java.nio.charset.StandardCharsets;
+
+import tw.com.fstop.util.Base64;
+
 /**
  * Hello world!
  *
@@ -9,5 +13,12 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Base64.Decoder decoder;
+        Base64.Encoder encoder = Base64.getEncoder();
+        final String text = "0123456789abcdefghijklmnopqrstuvwxyz ";
+        final byte[] encodeByte = text.getBytes(StandardCharsets.UTF_8);
+        final String encodedText = encoder.encodeToString(encodeByte);
+        System.out.println(encodedText);
+        
     }
 }
